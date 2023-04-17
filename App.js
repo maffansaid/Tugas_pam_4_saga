95ce7093e9fcc057178c454fb6a88213cd79ccd1
maffansaid <103346388+maffansaid@users.noreply.github.com>
2023-04-17 21:07:12 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Home from './screen/Home';
+import Penerbangan from './screen/Penerbangan';
+
+const Stack = createNativeStackNavigator();
+
+export default function App () {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }}/>
+        <Stack.Screen name="Penerbangan" component={Penerbangan} options={{ headerShown: false }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
